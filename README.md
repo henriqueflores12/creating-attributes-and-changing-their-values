a14da5a4096b046229e1b404976dfecff8358e4c
fagner henrique flores <henriquefagner@hotmail.com>
2020-03-15 12:58:03 -0500

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # creating-attributes-and-changing-their-values
+
+the className property allows you to change the values of the class
+attribute does not exist it will be created and given the specified value... more info at the js file...
---
+++ changingValuesattr/changingValuesattr.js
@@ -0,0 +1,7 @@
+var firstItem = document.getElementById("one"); //get the first item
+firstItem.className = "complete"; //change its class attribute
+
+var fourthItem = document.getElementsByTagName("li").item(3); //get fourth item
+fourthItem.setAttribute("class", "cool"); //add an attribute to it
+
+console.log(document);
